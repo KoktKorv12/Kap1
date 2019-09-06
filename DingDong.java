0a7bff7761177a7847c68568518303b8108513ff
Carl Berglund <carl.berglund@learnet.se>
2019-09-06 11:19:07 +0200

    test

---
+++ DingDong.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+
+public class DingDong {
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null,"Carl Berglund" + "\n" +
+			Calendar.getInstance().getTime().toString() ) ;
+	}
+}
---
+++ ProgrammeraDinKlasskamrat.java
@@ -0,0 +1,21 @@
+Program KoktKorv12 
+					?
+	ÖppnaÖgon() +
+	Snooza() +
+	StängAvVäckarklocka() +
+	KlivUpp() +
+	Toa(Duscha) +
+	Tvåla(Schampoo) +
+	Tvåla(FamilyFresh) +
+	Torka(Kropp) +
+	Torka (Hår)
+	KläPåDig() +
+	Frukost(Bröd) +
+	Bröd(Rosta) +
+	Bröd(Smör) +
+	Bröd(Ost) +
+	Bröd(skinka) +
+	Bröd(Ät) +
+	Toa(BorstaTänder) +
+	
+?
